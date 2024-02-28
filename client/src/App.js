d7a2a1ff8c93caf95b02053c4031b375bc6cf031
Samarth-Natrajan <samarthshridharnatrajan.bt21ece@pec.edu.in>
2024-02-28 11:03:59 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { ReactDOM } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import {Navbar} from './components/Navbar'
+import { Signin } from './pages/SignIn';
+import { Signup } from './pages/SignUp';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div className="App">
+        <BrowserRouter>
+        <Navbar/>
+      
+        <Routes>
+          <Route path='/signin' element = {<Signin />} />
+          <Route path = '/signup' element = {<Signup />}/>
+          <Route path='/'/>
+          
+        </Routes>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
