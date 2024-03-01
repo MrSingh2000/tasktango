64a56b381ed56974d774b99751aa70d98f607f91
Anshuman Singh <www.anshu2000@gmail.com>
2024-03-01 15:31:21 +0530

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import './App.css';
-import {Navbar} from './components/Navbar'
-import { Signin } from './pages/SignIn';
-import { Signup } from './pages/SignUp';
-import { DashBoard } from './pages/Dashboard';
-=======
 import "./App.css";
 import { Signin } from "./pages/SignIn";
 import { Signup } from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import NavContainer from "./components/NavContainer";
 import History from "./pages/History";
 import Tasks from "./pages/Tasks";
 import Settings from "./pages/Settings";
+import DashBoard from "./pages/Dashboard";
 
->>>>>>> 17cbdc9cd70f28b9a665d174021b49b7372e4e1c
 function App() {
   return (
     <div className="App">
@@ -35,14 +27,8 @@ function App() {
           theme="light"
         />
         <Routes>
-<<<<<<< HEAD
-          <Route path='/signin' element = {<Signin />} />
-          <Route path = '/signup' element = {<Signup />}/>
-          <Route path = '/dashboard' element={<DashBoard />}/>
-          
-=======
           <Route path="/" element={<NavContainer />}>
-            <Route path="" element={<Dashboard />} />
+            <Route path="" element={<DashBoard />} />
             <Route path="history" element={<History />} />
             <Route path="tasks" element={<Tasks />} />
             <Route path="settings" element={<Settings />} />
@@ -50,7 +36,6 @@ function App() {
 
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
->>>>>>> 17cbdc9cd70f28b9a665d174021b49b7372e4e1c
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,45 +1,40 @@
-<<<<<<< HEAD
 import { IoIosAddCircleOutline } from "react-icons/io";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-export const DashBoard = () =>{
-    const [currentDate, setCurrentDate] = useState(new Date());
+function Dashboard() {
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-        setCurrentDate(new Date());
-        }, 1000);    
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
     return () => clearInterval(intervalId);
-    }, []);
+  }, []);
 
-  
-  const formattedDateTime = currentDate.toLocaleString('en-US', {
-    day: 'numeric',
-    weekday: 'short',
-    month: 'short',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
+  const formattedDateTime = currentDate.toLocaleString("en-US", {
+    day: "numeric",
+    weekday: "short",
+    month: "short",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
     hour12: true,
   });
 
-    return (
-        <div className="mt-20 flex flex-col items-center w-full h-full">
-            <div className="flex flex-row w-full justify-between h-1/5"><div className="flex flex-col items-center justify-around w-3/12 md:w-2/12"><div className="">TODAY</div><div>{formattedDateTime}</div></div><div className="flex flex-row items-center justify-around w-3/12 h-1/5 md:w-2/12">Add New Task<IoIosAddCircleOutline/></div></div>
-        </div>
-    );
-}
-=======
-import React from "react";
-import { showToast } from "../helpers";
-
-function Dashboard() {
   return (
-    <div className="bg-green-200 w-full">
-      <button onClick={() => showToast("here")}>click me</button>
+    <div className="mt-20 flex flex-col items-center w-full h-full">
+      <div className="flex flex-row w-full justify-between h-1/5">
+        <div className="flex flex-col items-center justify-around w-3/12 md:w-2/12">
+          <div className="">TODAY</div>
+          <div>{formattedDateTime}</div>
+        </div>
+        <div className="flex flex-row items-center justify-around w-3/12 h-1/5 md:w-2/12">
+          Add New Task
+          <IoIosAddCircleOutline />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Dashboard;
->>>>>>> 17cbdc9cd70f28b9a665d174021b49b7372e4e1c
